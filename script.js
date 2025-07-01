eb431e2d4533c1a7c96f021b1b6a1693f1fcb395
Dennis <dennis-farris@hotmail.com>
2025-07-01 18:36:11 -0400

    Fixed a bug where journal area wasn't being cleared before updating view.  Fixed bug where edit was not replacing html properly.  Fixed display to match user's formatted display in terms of paragraph spacing and such.  Fixed issue where textarea on edit was not matching proper size.

--- script.js
+++ script.js
@@ -6,32 +6,43 @@ const journalEntryTemplate = document.getElementById("journal-entry");
 
 //checks if journal list exists, returns empty array if no object
 let journalList = JSON.parse(localStorage.getItem("journalList")) || [];
+console.log(journalList);
 
 //simple number-based id system for removal of items.
-
 let idCounter = localStorage.getItem("idCounter") || 0;
+
+//initial updateview call to populate blog list
+updateView();
+
+
+//saves data to journal list and id counter
+function saveData() {
+  localStorage.setItem("journalList", JSON.stringify(journalList));
+  localStorage.setItem("idCounter", idCounter);
+  console.log(journalList);
+}
 
 //updates journal section based on joural list
 function updateView() {
-  const fragment = new DocumentFragment();
+  let fragment = new DocumentFragment();
 
   for (let entry of journalList) {
     //creates a clone of html template for blog layout
-    console.log(entry);
     const newEntry = journalEntryTemplate.content.cloneNode(true);
 
     //sets dataset item for card id
     newEntry.querySelector(".journal-entry").dataset.id = entry.id;
 
-
     //creates the visible text elements
     newEntry.querySelector(".journal-title").textContent = entry.title;
     newEntry.querySelector(".journal-date").dateTime = entry.date;
     newEntry.querySelector(".journal-date").textContent = entry.date;
-    newEntry.querySelector(".journal-text").textContent = entry.text;
+    newEntry.querySelector(".journal-text").innerText = entry.text;
 
     fragment.appendChild(newEntry);
   }
+
+  journalContainer.innerHTML = "";
 
   journalContainer.appendChild(fragment);
 }
@@ -46,6 +57,7 @@ function validityCheck(object) {
   return true;
 }
 
+//EVENT LISTENERS//
 //event listener for form, checks validity of items and submits
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -64,5 +76,22 @@ form.addEventListener("submit", (event) => {
 
   idCounter++;
   journalList.push(newEntry);
+  saveData();
   updateView();
+});
+
+journalContainer.addEventListener("click", (event) => {
+  //edit button
+  if (event.target.classList.contains("edit-button")) {
+    //target current node
+    currentJournalEntry = event.target.closest("li");
+    currentJournalEntryText =
+      currentJournalEntry.querySelector(".journal-text");
+
+    let journalTextArea = document.createElement("textarea");
+    journalTextArea.className = "journal-text";
+    journalTextArea.textContent = currentJournalEntryText.textContent;
+
+    currentJournalEntryText.replaceWith(journalTextArea);
+  }
 });
